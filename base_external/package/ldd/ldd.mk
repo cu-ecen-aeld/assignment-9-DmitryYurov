92381bc2bd6fd26a04f10a7e4f8dc3d37cce2c4d
Dmitry Yurov <dm.yurov@gmail.com>
2024-03-03 22:50:03 +0100

    Fix building the modules

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,14 +9,7 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-DmitryYurov.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull modules
-endef
+LDD_MODULE_SUBDIRS = misc-modules scull
 
-#define LDD_INSTALL_TARGET_CMDS
-#	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-#	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-#endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
