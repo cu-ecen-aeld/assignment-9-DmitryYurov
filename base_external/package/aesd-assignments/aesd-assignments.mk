e4314252fa057b38714c8f9bf612d6dd34f3156b
Dmitry Yurov <dm.yurov@gmail.com>
2024-03-23 02:10:14 +0100

    Update commit references for the assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c1260da8eacbdbe91fadefd3df75de58942e392b
+AESD_ASSIGNMENTS_VERSION = a3fce87799671303a2adb6d807d7f365295af323
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -1,7 +1,7 @@
 #
 ##############################################################
 
-AESD_CHAR_DRIVER_VERSION = ba3a817bcfbdd9aea9baf7a896ff10454a8a8f38
+AESD_CHAR_DRIVER_VERSION = a3fce87799671303a2adb6d807d7f365295af323
 AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-DmitryYurov.git
 AESD_CHAR_DRIVER_SITE_METHOD = git
 AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
